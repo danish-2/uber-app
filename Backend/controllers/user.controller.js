ef508426469677b5fb6a73071afe0d669df88653
Danish Khan <mohammeddanishkhan98@gmail.com>
2025-02-02 11:57:31 +0530

    userlogin done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -24,3 +24,27 @@ module.exports.registerUser = async (req, res, next) => {
 
     res.status(201).json({user, token});
 }
+
+module.exports.loginUser = async (req, res, next) => {
+    const error = await validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(400).json({error: error.array()});
+    }
+    
+    const {email, password} = req.body;
+    const user = await userModel.findOne({email}).select('+password');
+
+    if(!user){
+        return res.status(401).json({error: 'Invalid Email or Password'});
+    }
+    
+    const isMatch = await user.comparePassword(password);
+    
+    if(!isMatch){
+        return res.status(401).json({error: 'Invalid Email or Password'});
+    }
+    
+    const token = user.generateAuthToken();
+    
+    res.status(200).json({token, user});
+}
